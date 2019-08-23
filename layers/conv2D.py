53fcd1d0ec4a2df4717c1250bfd2e6073f66bfbe
jzhang9100 <jz9100@jacks-desktop.localdomain>
2019-08-23 02:57:35 -0400

    updates conv

--- layers/conv2D.py
+++ layers/conv2D.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 '''
 Takes in tensor 4-D  tensor with dimensions Length_Of_Data x Rows x Cols x Channels and performs a convolution resulting in
@@ -5,9 +6,19 @@ a new 4-D tensor with dimensions Length_Of_Data x newRows x newCols x Filters
 '''
 
 class Conv2D:
-    def __init__(self, tensor, filters, kernel_size, stride):
+    def __init__(self, tensor, filters, kernel_size): 
         self.filters = filters
-        (self.kernel_X, self.kernel_Y)  = kernel_size
-        self.stride = stride
         self.tensor = tensor
+        #get input size of image
+        print(tensor.shape, tensor)
+        print(self.tensor.shape, self.tensor)
+        self.x, self.y, self.z = self.tensor.shape
+        #establish kernel dimensions and assert that the kernel is a square
+        (self.kernel_X, self.kernel_Y)  = kernel_size
+        assert self.kernel_X == self.kernel_Y
         
+        self.kernel_Z = self.z
+        assert self.kernel_Z == len(self.tensor[0][0]) 
+
+        self.kernel = [[np.randn(0, 255)]*self.kernel_X]
+        print(self.kernel)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from keras.datasets import cifar10
+from layers.conv2D import Conv2D
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print(x_train.shape)
 
+test = x_train[0]
+conv_test = Conv2D(test, 64, (2,2))
