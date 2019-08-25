689c960bfb3e9f4679d18a5df0b2bd4979435471
jzhang9100 <jz9100@jacks-desktop.localdomain>
2019-08-25 18:49:50 -0400

    activation.py applies a non linearity (ReLU)

---
+++ layers/activation.py
@@ -0,0 +1,16 @@
+'''
+Takes in tensor and applies an activation function (only using ReLU right now but more to add). Returns tensor with activation function applied
+'''
+
+class Activation:
+    def __init__(self, tensor):
+        self.tensor = tensor
+        self.activated_tensor = self.relu()
+
+    def relu(self):
+        t = self.tensor
+        max_fn = lambda x : max(0, x)  #ReLU function
+        for x, d in enumerate(t):
+            for i, l in enumerate(d):
+                t[x][i] = list(map(max_fn, l))
+        print(t)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from keras.datasets import cifar10
 from layers.conv2D import Conv2D
+from layers.activation import Activation
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print(x_train.shape)
 
+x_train, x_test = x_train/255, x_test/255
 test = x_train[0]
 print(test)
-conv_test = Conv2D(test, 64, (3,3))
+#conv_test = Conv2D(test, 64, (3,3))
+#conv_test.get_kernel_info()
+
+activation_test = Activation(test)
