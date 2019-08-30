fbce63058a7807e62a9c6305ba4ca83364d9dcb6
jzhang9100 <jz9100@jacks-desktop.localdomain>
2019-08-29 20:18:59 -0400

    nuerons are now fully connected with a sigmoid activation

--- layers/dense.py
+++ layers/dense.py
@@ -1,14 +1,9 @@
 import numpy as np
 
-class sigmoid:
-    def __init__(self, z):
-        self.z = z #input 
-        
+def sigmoid(z): 
         #put z into sigmoid function
-        self.sigmoid_function = 1 / (1 + np.exp(-self.z))
-    
-    def get_output(self):
-        return self.sigmoid_function
+        sigmoid_function = 1 / (1 + np.exp(-z))
+        return sigmoid_function
 
 #fully connected layer, takes activations and implements a neural network to compute class scores
 class Dense:
@@ -18,10 +13,16 @@ class Dense:
         #weights initiated to be random matrix of neurons x flattened input
         self.weights =  np.random.randint(-1, 2, (self.neurons, len(self.flat)))   
         self.bias = np.random.randint(-1, 2, (self.neurons, 1)) #random bias vecotor with mean 0 and variance 1
+        self.out = self.fowardpass()  #pass input, wieghts, and bias through sigmoid function
+
+        print('connected', self.out, np.shape(self.out))
         
-        self.fowardpass()
 
     def fowardpass(self):
-        print('weights', self.weights, self.weights.shape)
-        print('bias', self.bias, self.bias.shape)
-        
+        out  = []
+
+        assert len(self.bias) == len(self.weights) #make sure matrix multiplication finna work
+        for i in range(self.neurons):
+            z = np.dot(self.flat, self.weights[i]) + self.bias[i]  #sigmoid gate
+            out.append(sigmoid(z[0]))
+        return out
