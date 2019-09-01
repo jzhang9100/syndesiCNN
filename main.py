809537c270ecf62ca0d16ae24765e12d630ef0ee
jzhang9100 <jackzhang9100@yahoo.com>
2019-09-01 13:10:28 -0400

    train model with minibatches

--- main.py
+++ main.py
@@ -5,5 +5,22 @@ from model import Sequential
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 x_train, x_test = x_train/255, x_test/255
+
 model = Sequential(x_train, y_train)
-model.addConv(32, (3,3))
+
+model.add_Conv(32, (3,3))
+model.add_Activation()
+model.add_Pool()
+
+model.add_Conv(32, (3,3))
+model.add_Activation()
+model.add_Pool()
+
+model.add_Conv(64, (3,3))
+model.add_Activation()
+model.add_Pool()
+
+model.add_Dense(512)
+model.add_Dense(10)
+model.show()
+
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from layers.conv2D import Conv2D
-from layers.activation import Relu
+from layers.activation import Activation
 from layers.dense import Dense
 from layers.pool import MaxPool
 
@@ -7,22 +7,46 @@ class Sequential:
     def __init__(self, train_data, train_labels):
         assert len(train_data) == len(train_labels)
         self.data = train_data
+        self.dat_size = len(self.data)
         self.labels = train_labels
         self.layers = []
-
+    
+    #adds conv layer with parameters into layer queue
     def add_Conv(self, filters, kernel_size):
         layer = "conv," + str(filters) + "," + str(kernel_size)
-        print(layer)
         self.layers.append(layer)
-
+    
+    #adds an activation layer to layer queue
     def add_Activation(self):
         layer = "activation"
         self.layers.append(layer)
-
+    
+    #adds a max pooling layer to the layer queue
     def add_Pool(self):
         layer = "pool"
         self.layers.append(layer)
 
+    #adds a fully connected layer with specified nuerons to layer queue
     def add_Dense(self, neurons):
         layer = "dense," + str(neurons)
         self.layers.append(layer)
+    
+    #prints layer queue
+    def show(self):
+        for layer in self.layers:
+            print(layer)
+    
+    '''
+    trains the model with user given epochs and batch sizes
+    can have a vaildation split if specified, other wise default is none
+    '''
+    def compile(self, epochs, batchSize, validation_split = None):
+        for epoch in epochs:
+            batches = self.dat_size//batchSize
+            for batch in batches:
+                curr_batch = None
+                if batch == batches:
+                    curr_batch = [self.dat_size:self.dat_size - (batch*batchSize)] 
+                else:
+                    curr_batch = self.data[batch*batchSize:batch*(batchSize-1)] #deffintly a bertter way to do this lol
+                print(curr_batch.shape)
