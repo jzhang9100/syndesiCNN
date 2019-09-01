921520ecda3b97d8e49fa1956b08c0a3b31d7b13
jzhang9100 <jackzhang9100@yahoo.com>
2019-09-01 11:42:28 -0400

    sequential model

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
 from keras.datasets import cifar10
-from layers.conv2D import Conv2D
-from layers.activation import Activation
-from layers.pool import MaxPool
-from layers.dense import Dense
+from model import Sequential
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-print(x_train.shape)
 
 x_train, x_test = x_train/255, x_test/255
-test = x_train[0]
-print(test)
-conv_test = Conv2D(test, 64, (3,3))
-#conv_test.get_kernel_info()
-
-activation_test = Activation(conv_test.conv)
-
-pool_test = MaxPool(activation_test.activated_tensor)
-
-layer1 = Dense(512, pool_test.dwn_smpl, 2)
-layer2 = Dense(10, layer1.out, 2)
-layer2.show()
-print(y_train[0])
+model = Sequential(x_train, y_train)
+model.addConv(32, (3,3))
--- model.py
+++ model.py
@@ -1,13 +1,14 @@
-from dense import Dense
-from conv2D import Conv2D
-from max_pooling import Maxpool
-from normalization import batch_norm
+from layers.conv2D import Conv2D
 
 
 class Sequential:
-    def __init__(self):
+    def __init__(self, train_data, train_labels):
+        assert len(train_data) == len(train_labels)
+        self.data = train_data
+        self.labels = train_labels
         self.layers = []
 
-    def addDense(self, units):
-
-
+    def addConv(self, filters, kernel_size):
+        layer = "conv," + str(filters) + "," + str(kernel_size)
+        print(layer)
+        self.layers.append(layer)
