3d65cc86142f806e889d8da4e566a4fa8687dd08
jzhang9100 <jackzhang9100@yahoo.com>
2019-11-24 15:56:34 -0500

    batching

--- backprop/loss.py
+++ backprop/loss.py
@@ -1,16 +1,22 @@
-import numpy as np
+from math import log
 
 class Loss:
     '''
-    y = binary labeld array
+    y = onehot label'd scores
     p = predicted probability 
     '''
     def __init__(self, y, p):
         assert len(y) == len(p)
-        self.cross_entropy = self.CrossEntropy(y, p)
+        self.y = y
+        self.p = p
+        self.cross_entropy = self.CrossEntropy(self.y, self.p)
 
     def CrossEntropy(self, y, p):
-        loss = 0
+        sum_score = 0.0 
+        assert len(y) == len(p), "input arrays error, lengths dont match"
+        
         for i in range(len(y)):
-            loss += np.nan_to_num(-y[i]*np.log(p[i])-(1-y[i])*np.log(1-p[i]))
-        return loss
+            for j in range(len(y[0])):
+                sum_score += y[i][j] * (log(1e-15 + p[i][j]))
+        mean_sum_score = (1.0 / len(y)) * sum_score
+        return -mean_sum_score
